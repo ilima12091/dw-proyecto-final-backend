ed927d60928b04de80e1168e324b1fe62980a98d
RobottiFranco <franco.robotti0@gmail.com>
2023-07-01 18:24:14 -0300

    add delete post endpoint

--- index.ts
+++ index.ts
@@ -22,33 +22,6 @@ app.get("/", (req: Request, res: Response) => {
 // Array para almacenar los posts (simulación de una base de datos)
 const posts: Post[] = [];
 
-// Nuevo endpoint para crear un nuevo post
-app.post('/users/:username/posts', (req: Request, res: Response) => {
-  const { username } = req.params;
-  const { title, content } = req.body as Post;
-
-  //
-  //
-  //lógica para crear un nuevo post
-  //
-  //
-
-
-  if (username) {
-    // Si el usuario está autenticado, crea el nuevo post
-    const postId = String(posts.length + 1); // ID del nuevo post creado
-    const post: Post = { postId, title, content, username };
-    posts.push(post);
-    res.status(200).json(post);
-  } else {
-    // Si el usuario no está autenticado, devuelve el error 401
-    res.status(401).json({
-      errorCode: 'unauthenticated',
-      errorMessage: 'Usuario no autenticado'
-    });
-  }
-});
-
 // Endpoint para eliminar un post existente
 app.delete('/users/:username/posts/:postId', (req: Request, res: Response) => {
   const { username, postId } = req.params;
