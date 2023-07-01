d40e6d41de0716cb3104149168c7b9cac59d5b5a
RobottiFranco <franco.robotti0@gmail.com>
2023-07-01 20:42:08 -0300

    add modify user endpoint

--- index.ts
+++ index.ts
@@ -19,52 +19,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 
-// Endpoint para actualizar los datos de perfil de un usuario
-app.put('/users/:username', (req: Request, res: Response) => {
-  const { username } = req.params;
-  const { name, surname, email, password } = req.body as User;
-
-
-  // la lista de usuarios esta en routes, no pude traerla
-
-  // Busca el usuario por su nombre de usuario
-  const user = users.find(user => user.username === username);
-
-  if (user) {
-    // Si el usuario existe, actualiza sus propiedades
-    if (name) {
-      user.name = name;
-    }
-    if (surname) {
-      user.surname = surname;
-    }
-    if (email) {
-      user.email = email;
-    }
-    if (password) {
-      user.password = password;
-    }
-    if (username) {
-      user.username = username;
-    }
-
-    res.status(200).json({
-      id: user.id,
-      name: user.name,
-      surname: user.surname,
-      email: user.email,
-      userName: user.username,
-      profilePicture: user.profilePicture
-    });
-  } else {
-    // Si el usuario no existe, devuelve el error 404
-    res.status(404).json({
-      errorCode: 'user_not_found',
-      errorMessage: 'Usuario no existe'
-    });
-  }
-});
-
 /*
   app.post('/register',(req,res)=>{
   const{name,surname,email,password,username}=req.body;
--- routes/user.ts
+++ routes/user.ts
@@ -17,6 +17,45 @@ router.post("/register",(req,res)=>{
 
 });
 
-
+// Endpoint para actualizar los datos de perfil de un usuario
+router.put('/users/:username', (req, res) => {
+    const { username, name, surname, email, password } = req.body;
+  
+    // Busca el usuario por su nombre de usuario
+    const user = users.find(user => user.username === username);
+  
+    // Si el usuario existe, actualiza sus propiedades
+    if (user) {
+      if (name) {
+        user.name = name;
+      }
+      if (surname) {
+        user.surname = surname;
+      }
+      if (email) {
+        user.email = email;
+      }
+      if (password) {
+        user.password = password;
+      }
+      if (username) {
+        user.username = username;
+      }
+  
+      res.status(200).json({
+        name: user.name,
+        surname: user.surname,
+        email: user.email,
+        userName: user.username,
+    
+    });
+    } else {
+      // Si el usuario no existe, devuelve el error 404
+      res.status(404).json({
+        errorCode: 'user_not_found',
+        errorMessage: 'Usuario no existe'
+      });
+    }
+  });
 
 export default router;
