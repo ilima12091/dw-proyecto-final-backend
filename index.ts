3d85df2f99d833dda3f3ca8994960a3c4d7fe209
RobottiFranco <franco.robotti0@gmail.com>
2023-07-01 18:57:25 -0300

    add modify user endpoint

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user";
+import { User } from "./interface/user";
 dotenv.config();
 
 const app: Express = express();
@@ -17,6 +18,52 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+
+// Endpoint para actualizar los datos de perfil de un usuario
+app.put('/users/:username', (req: Request, res: Response) => {
+  const { username } = req.params;
+  const { name, surname, email, password } = req.body as User;
+
+
+  // la lista de usuarios esta en routes, no pude traerla
+
+  // Busca el usuario por su nombre de usuario
+  const user = users.find(user => user.username === username);
+
+  if (user) {
+    // Si el usuario existe, actualiza sus propiedades
+    if (name) {
+      user.name = name;
+    }
+    if (surname) {
+      user.surname = surname;
+    }
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+    if (username) {
+      user.username = username;
+    }
+
+    res.status(200).json({
+      id: user.id,
+      name: user.name,
+      surname: user.surname,
+      email: user.email,
+      userName: user.username,
+      profilePicture: user.profilePicture
+    });
+  } else {
+    // Si el usuario no existe, devuelve el error 404
+    res.status(404).json({
+      errorCode: 'user_not_found',
+      errorMessage: 'Usuario no existe'
+    });
+  }
+});
 
 /*
   app.post('/register',(req,res)=>{
