422c669ab707c691d801d9db0e809cbb79779501
Valeria <valecardozog@gmail.com>
2023-07-01 10:39:59 -0300

    updates on missing fields, and navigates to login only when user is created

--- index.ts
+++ index.ts
@@ -1,26 +1,33 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
+import userRoutes from "./routes/user";
 dotenv.config();
-const bodyParser= require('body-parser');
-const cors= require('cors');
 
 const app: Express = express();
-app.use(cors());
 const port = process.env.PORT;
 
+const bodyParser= require('body-parser');
+const cors= require('cors');
+app.use(cors());
 app.use(bodyParser.json());
+
+app.use('/user',userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.post('/register',(req,res)=>{
+
+/*
+  app.post('/register',(req,res)=>{
   const{name,surname,email,password,username}=req.body;
 
   res.status(200).json({success:true,message:'User registered succesfully'});
   console.log(req.body); //just to try and see if it works
 })
+ 
+ */
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- routes/user.ts
+++ routes/user.ts
@@ -5,6 +5,18 @@ const router= Router();
 
 const users: User[] = [];
 
+router.post("/register",(req,res)=>{
+    const newUser: User =req.body;
+    if (!newUser.name || !newUser.surname || !newUser.email || !newUser.password || !newUser.username) {
+        return res.status(400).json({ success: false, message: "All fields are required" });
+    }
+    
+    users.push(newUser);
+    res.status(200).json({success:true,message:'User registered succesfully'});
+    console.log("users: ",users); //just to try and see if it works
+
+});
+
 
 
 export default router;
