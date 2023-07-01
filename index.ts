6d3ae65394857d63f20159aad541d600195be254
Valeria <valecardozog@gmail.com>
2023-07-01 09:43:09 -0300

    basic user registration

--- index.ts
+++ index.ts
@@ -2,14 +2,27 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+const bodyParser= require('body-parser');
+const cors= require('cors');
 
 const app: Express = express();
+app.use(cors());
 const port = process.env.PORT;
+
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+app.post('/register',(req,res)=>{
+  const{name,surname,email,password,username}=req.body;
+
+  res.status(200).json({success:true,message:'User registered succesfully'});
+  console.log(req.body); //just to try and see if it works
+})
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
---
+++ routes/user.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { User } from "../interface/user";
+
+const router= Router();
+
+const users: User[] = [];
+
+
+
+export default router;
