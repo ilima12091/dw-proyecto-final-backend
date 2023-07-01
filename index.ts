699ff0fd31a1b665a0f85067dc1d36d6a9502d8c
RobottiFranco <franco.robotti0@gmail.com>
2023-07-01 18:23:35 -0300

    add patch post endpoint

--- index.ts
+++ index.ts
@@ -22,52 +22,6 @@ app.get("/", (req: Request, res: Response) => {
 // Array para almacenar los posts (simulación de una base de datos)
 const posts: Post[] = [];
 
-// Nuevo endpoint para crear un nuevo post
-app.post('/users/:username/posts', (req: Request, res: Response) => {
-  const { username } = req.params;
-  const { title, content } = req.body as Post;
-
-  //
-  //
-  //lógica para crear un nuevo post
-  //
-  //
-
-
-  if (username) {
-    // Si el usuario está autenticado, crea el nuevo post
-    const postId = String(posts.length + 1); // ID del nuevo post creado
-    const post: Post = { postId, title, content, username };
-    posts.push(post);
-    res.status(200).json(post);
-  } else {
-    // Si el usuario no está autenticado, devuelve el error 401
-    res.status(401).json({
-      errorCode: 'unauthenticated',
-      errorMessage: 'Usuario no autenticado'
-    });
-  }
-});
-
-// Endpoint para eliminar un post existente
-app.delete('/users/:username/posts/:postId', (req: Request, res: Response) => {
-  const { username, postId } = req.params;
-
-  // Busca el post por su ID y usuario asociado
-  const index = posts.findIndex(post => post.postId === postId && post.username === username);
-  if (index !== -1) {
-    // Si se encuentra el post, elimínalo del array de posts
-    posts.splice(index, 1);
-    res.status(200).send("Post has been deleted successfully");
-  } else {
-    // Si el post no existe, devuelve el error 404
-    res.status(404).json({
-      errorCode: 'post_not_found',
-      errorMessage: 'El post no existe'
-    });
-  }
-});
-
 // Endpoint para modificar un post existente
 app.put('/users/:username/posts/:postId', (req: Request, res: Response) => {
   const { username, postId } = req.params;
